e96f81001528ff5e7f719dcfabbcd51c9f364190
Janice <janicelaset@gmail.com>
2015-08-14 10:06:34 -0700

    save words to the word list

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,14 +4,24 @@ require('word')
 describe(Word) do
   describe('#word') do
     it('returns the word that is entered') do
-      test_word_list = Word.new("Yo")
-      expect(test_word_list.word()).to(eq("Yo"))
+      test_word = Word.new("Yo")
+      expect(test_word.word()).to(eq("Yo"))
     end
   end
 
   describe('.all') do
     it('returns a list of words that is empty at first') do
       expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves a word to the word list') do
+      test_word = Word.new("Yo")
+      test_word.save()
+      test_word2 = Word.new("Splendiferous")
+      test_word2.save()
+      expect(Word.all).to(eq([test_word, test_word2]))
     end
   end
 end
