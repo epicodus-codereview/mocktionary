3de7c439029a2a21adfe0741fa41e51c80ac2cad
Janice <janicelaset@gmail.com>
2015-08-14 09:33:36 -0700

    change word method to receive a word as a parameter and return the word

--- lib/word.rb
+++ lib/word.rb
@@ -2,7 +2,7 @@ class Word
   attr_reader(:word)
   @@words = []
 
-  define_method(:initialize) do
-    @word = self
+  define_method(:initialize) do |word|
+    @word = word
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,8 +4,9 @@ require('word')
 describe(Word) do
   describe('#word') do
     it('returns the word that is entered') do
-      test_word = Word.new()
-      expect(test_word.word()).to(eq(test_word))
+      test_word_list = Word.new("Yo")
+      expect(test_word_list.word()).to(eq("Yo"))
     end
   end
+
 end
