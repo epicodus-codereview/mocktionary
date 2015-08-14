6315fb1b499198ed15a9286a42ab6ed106768c34
Janice <janicelaset@gmail.com>
2015-08-14 16:16:18 -0700

    return a definition by its id number

--- lib/definition.rb
+++ lib/definition.rb
@@ -5,7 +5,7 @@ class Definition
 
   define_method(:initialize) do |definition|
     @definition = definition
-    @id = @@definitions.length
+    @id = @@definitions.length + 1
   end
 
   define_singleton_method(:all) do
@@ -22,5 +22,13 @@ class Definition
 
   define_method(:delete) do
     @@definitions.delete(self)
+  end
+
+  define_singleton_method(:find) do |id_number|
+    @@definitions.each do |definition|
+      if definition.id == id_number.to_i
+        return definition
+      end
+    end
   end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -50,14 +50,24 @@ describe(Definition) do
       expect(Definition.all).to(eq([test_def2]))
     end
   end
-end
 
-describe('#id') do
-  it('returns the id number of a definition on the list') do
-    test_def = Definition.new("Fear of the number 10")
-    test_def.save
-    test_def2 = Definition.new("A condition characterized by freedom from worry or any other pre-occupation of reality")
-    test_def2.save
-    expect(test_def.id()).to(eq(1))
+  describe('#id') do
+    it('returns the id number of a definition on the list') do
+      test_def = Definition.new("Fear of the number 10")
+      test_def.save
+      test_def2 = Definition.new("A condition characterized by freedom from worry or any other pre-occupation of reality")
+      test_def2.save
+      expect(test_def.id()).to(eq(1))
+    end
+  end
+
+  describe('.find') do
+    it('returns a definition by its id number') do
+      test_def = Definition.new("Fear of the number 10")
+      test_def.save
+      test_def2 = Definition.new("A condition characterized by freedom from worry or any other pre-occupation of reality")
+      test_def2.save
+      expect(Definition.find(test_def2.id())).to(eq(test_def2))
+    end
   end
 end
