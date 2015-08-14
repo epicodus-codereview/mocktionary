43ff2778f3837b28ae060a576ead41a886b801dc
Janice <janicelaset@gmail.com>
2015-08-14 10:36:36 -0700

    return a list of definitions that is empty at first

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,6 +1,13 @@
 class Definition
   attr_reader(:definition)
+
+  @@definitions = []
+
   define_method(:initialize) do |definition|
     @definition = definition
+  end
+
+  define_singleton_method(:all) do
+    @@definitions
   end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -8,4 +8,10 @@ describe(Definition) do
       expect(test_definition.definition).to(eq("Fear of the number 10"))
     end
   end
+
+  describe(".all") do
+    it('returns a list of definitions that is empty at first') do
+      expect(Definition.all).to(eq([]))
+    end
+  end
 end
